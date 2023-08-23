fb5cbb6c693033447d0874eaa179455f95e23ae6
paulo <pbrsj1385@gmail.com>
2023-08-23 13:49:26 -0300

    Iniciadas as entidades Pessoas e Tarefas

---
+++ src/main/java/br/com/testecrja/gerenciamentotarefas/entidades/Pessoas.java
@@ -0,0 +1,46 @@
+package br.com.testecrja.gerenciamentotarefas.entidades;
+
+public class Pessoas {
+
+	private Long id;
+	private String nome;
+	private String Departemento;
+	private String ListaDeTarefas;
+
+	public Pessoas() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDepartemento() {
+		return Departemento;
+	}
+
+	public void setDepartemento(String departemento) {
+		Departemento = departemento;
+	}
+
+	public String getListaDeTarefas() {
+		return ListaDeTarefas;
+	}
+
+	public void setListaDeTarefas(String listaDeTarefas) {
+		ListaDeTarefas = listaDeTarefas;
+	}
+
+}
---
+++ src/main/java/br/com/testecrja/gerenciamentotarefas/entidades/Tarefa.java
@@ -0,0 +1,93 @@
+package br.com.testecrja.gerenciamentotarefas.entidades;
+
+public class Tarefa {
+
+	private Long id;
+	private String Titulo;
+	private String Descricao;
+	private String Prazo;
+	private String Departamento;
+	private String Duracao;
+	private String PessoaAlocada;
+	private String Finalizado;
+	
+	private Pessoas pessoas;
+	
+	public Tarefa() {
+		
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return Titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return Descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+
+	public String getPrazo() {
+		return Prazo;
+	}
+
+	public void setPrazo(String prazo) {
+		Prazo = prazo;
+	}
+
+	public String getDepartamento() {
+		return Departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		Departamento = departamento;
+	}
+
+	public String getDuracao() {
+		return Duracao;
+	}
+
+	public void setDuracao(String duracao) {
+		Duracao = duracao;
+	}
+
+	public String getPessoaAlocada() {
+		return PessoaAlocada;
+	}
+
+	public void setPessoaAlocada(String pessoaAlocada) {
+		PessoaAlocada = pessoaAlocada;
+	}
+
+	public String getFinalizado() {
+		return Finalizado;
+	}
+
+	public void setFinalizado(String finalizacao) {
+		Finalizado = finalizacao;
+	}
+
+	public Pessoas getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(Pessoas pessoas) {
+		this.pessoas = pessoas;
+	}
+
+}
